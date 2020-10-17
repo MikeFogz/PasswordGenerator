6e4db9a147fc61ffb42b683ba4aa3e1d33d6f2b2
MikeFogz <MikeFogz@gmail.com>
2020-10-17 15:40:11 -0700

    added in length prompt & check followed by confirms for pw criteria

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-var char = ["A", "B", "C", "D", "E", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var upperChars = ["A", "B", "C", "D", "E", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 //can I list all caps and then use toLowerCase to pull a lower case version?
-var specChar = ["!","#", "$", "%", "&", "(", ")", "*". "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "^", "_", "{", "|", "}", "~"]
+var specChars = ["!","#", "$", "%", "&", "(", ")", "*". "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "^", "_", "{", "|", "}", "~"]
 var emoji = ["😀", "🤯", "🧐", "😎"]
 
 
@@ -17,8 +17,19 @@ function writePassword() {
 
   if (length < 8 || length > 128 || isNaN(length)) {
     alert("Not accepted. Must be between 8-128 characters.")
+  } else{
+    var lowerTrue = confirm("Do you want lower case letters?");
+    var upperTrue = confirm("Do you want Upper Case letters?");
+    var numsTrue = confirm("Do you want numbers?");
+    var sCharsTrue = confirm("Do you want Special Characters?");
+    var emojiTrue = confrim("Do you want emojis?");
+
+    if (lowerTrue)
+    if (upperTrue)
+
   }
 }
+
 
 // let lowerCase = confirm("Do you want lowercase?"){
 //   if (lowerCase) {
